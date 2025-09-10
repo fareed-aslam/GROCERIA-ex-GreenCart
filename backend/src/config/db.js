18fe27efc64d9a515a4552592b43774dcbbfb36f
Muhammad Fareed <mfareedmughalinfo@gmail.com>
2025-09-11 01:33:05 +0500

    Fix database connection handling and update Vercel configuration paths

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
+export let isConnected = false;
+
 export const connectDb = async () => {
   try {
     const connnectionInstance = await mongoose.connect(
       `${process.env.MONGO_URI}/${DB_NAME}`
     );
+
+    isConnected = true;
     console.log(
       `MONGODB connected || DB Host is ${connnectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("DB erro", error);
+    console.log("DB error", error);
     process.exit(1);
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,27 @@
 import { app } from "./app.js";
-import { connectDb } from "./config/db.js";
+import { connectDb, isConnected } from "./config/db.js";
 import dotenv from "dotenv";
 import { connectCloudinary } from "./utils/cdn.cloudinary.js";
 
-dotenv.config({
-  path: "./.env",
-  debug: true,
-});
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-
 await connectCloudinary();
-await connectDb()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("connection failed...", error);
-  });
+// await connectDb()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server is running at http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("connection failed...", error);
+//   });
+
+app.use((req, res) => {
+  if (!isConnected) {
+    connectDb();
+  }
+});
+
+export default app;
